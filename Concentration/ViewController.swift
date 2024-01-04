34e9325cdd4785fdfc0b132a687fb93d2e9887b4
intelli.hill <intelli.hill@outlook.com>
2024-01-05 06:06:43 +0900

    study : property observer, array

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,13 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    // _ : 인수가 없다는 뜻
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["😀","😍","🤩","😛"]
+    
+    var flipCount: Int = 0 {
+        // Property Observer
+        // 속성이 설정될 때마다 이 안의 코드가 실행 됨
+        didSet {
+            flipCountLabel.text = "Flips : \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    // _ : 인수가 없다는 뜻(no argument)
     // Object-c에서 온 기능이고, 거기에는 내부 외부 이름이 없어서 _가 쓰임
     @IBAction func touchCard(_ sender: UIButton) {
         
         // 영어처럼 읽혀야함 flipCard withEmoji on sender
-        flipCard(withEmoji: "😍", on: sender)
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardNumber : \(cardNumber)")
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
@@ -25,7 +45,7 @@ class ViewController: UIViewController {
             button.backgroundColor = UIColor.black
         } else {
             button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor = UIColor.orange
+            button.backgroundColor = UIColor.white
         }
     }
     
