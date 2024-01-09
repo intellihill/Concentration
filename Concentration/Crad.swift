6a5dae1055eff55f8b2b3e82bc74e7a2adbb6231
intelli.hill <intelli.hill@outlook.com>
2024-01-09 21:31:58 +0900

    study: stack_view

--- Concentration/Crad.swift
+++ Concentration/Crad.swift
@@ -38,3 +38,12 @@ struct Card
         self.identifier = Card.getUniqueIdentifier()
     }
 }
+struct Person: Hashable {
+    var name: String
+    var age: Int
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(age)
+    }
+}
